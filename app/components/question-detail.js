90fd082f3c0d2e7511af5a6e540d8719d1ec82d3
Amber Farrington <amberf0712@outlook.com>
2016-11-03 17:59:21 -0700

    add actions to delete, update, and save answer and question

--- app/components/question-detail.js
+++ app/components/question-detail.js
@@ -1,4 +1,20 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    deleteQuestionHandler(question){
+      if(confirm('Are you sure you want to delete this question?')) {
+        this.sendAction('deleteQuestionSender', question);
+      },
+      deleteAnswerSender(answer) {
+        this.sendAction('deleteAnswerSender', answer);
+      },
+      updateQuestionSender(question, params) {
+        this.sendAction('updateQuestionSender', question, params);
+      },
+      saveAnswerSender(params) {
+        this.sendAction('saveAnswerSender', params);
+      }
+    }
+  }
 });
