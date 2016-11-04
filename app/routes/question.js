af61b35c8b854cfd7d9c820004901b016c579cbb
Amber Farrington <amberf0712@outlook.com>
2016-11-03 17:20:31 -0700

    add actions to the question route

--- app/routes/question.js
+++ app/routes/question.js
@@ -1,4 +1,38 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  model(params){
+    return this.store.findRecord('question',params.question_id);
+  },
+  actions: {
+    deleteQuestion(question){
+      var answer_deletions = question.get('answers', params.question_id);
+        return answer.destroyRecord();
+    });
+    Ember.RSVP.all(answer_deletions).then(function() {
+      return question.destroyRecord();
+    });
+    this.transitionTo('index');
+  },
+  updateQuestion(question, params){
+    Object.keys(params).forEach(function(key){
+      if(params[key]!==undefined) {
+        question.set(key,params[key]);
+      }
+    });
+    question.save();
+  },
+  saveAnswer(params){
+    var newAnswer = this.store.createRecord('answer',params);
+    var question = params.question;
+    question.get('answers').addObject(newAnswer);
+    newAnswer.save().then(function() {
+      return question.save();
+    });
+    this.transitionTo('question',question);
+  },
+  deleteAnswer(answer) {
+    answer.destroyRecord();
+    }
+  }
 });
