28c585a4cdd0348d90688dc335b8e97efdde7b51
Amber Farrington <amberf0712@outlook.com>
2016-11-03 18:24:29 -0700

    add method calls to answer-tile and new-answer JS

--- app/components/answer-tile.js
+++ app/components/answer-tile.js
@@ -1,4 +1,11 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions:{
+    deleteAnswerHandler(answer) {
+      if (confirm('Are you sure you want to delete this answer?')) {
+        this.sendAction('deleteAnswerSender', answer);
+      }
+    }
+  }
 });
--- app/components/new-answer.js
+++ app/components/new-answer.js
@@ -1,4 +1,22 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  addNewAnswer: false,
+  actions: {
+    AnswerFormShow() {
+      this.set('addNewAnswer', true);
+    },
+    cancelSaveAnswer(){
+      this.set('addNewAnswer', false);
+    },
+    saveAnswerHandler() {
+      var params = {
+        author: this.get('author'),
+        content: this.get('content'),
+        question: this.get('question')
+      };
+      this.set('addNewAnswer', false);
+      this.sendAction('saveAnswerSender', params);
+    }
+  }
 });
