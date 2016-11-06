90b4cd11021e14ca5ccce40e87ed31161d8f5ece
Amber Farrington <amberf0712@outlook.com>
2016-11-05 19:00:08 -0700

    Update question-tile.js

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   actions: {
     favoriteQuestionHandler(question)
     this.sendAction('favoriteQuestionHandler', question);
-  });
+  },
 
   numberOfAnswers: Ember.computed('question.answers', function() {
     return this.get('question.answers').content.length;
