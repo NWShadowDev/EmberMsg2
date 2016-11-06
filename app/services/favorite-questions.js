175095a392d66dd54909208111ea7086d3dd4bd8
Amber Farrington <amberf0712@outlook.com>
2016-11-05 20:06:37 -0700

    get rid of extra curly brace

--- app/services/favorite-questions.js
+++ app/services/favorite-questions.js
@@ -4,7 +4,6 @@ export default Ember.Service.extend({
   questions: [],
   add(question){
     this.get('questions').pushObject(question)
-  }
   },
   remove(question){
     this.get('questions').removeObject(question)
