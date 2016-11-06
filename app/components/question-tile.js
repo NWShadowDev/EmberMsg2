c29fa38e2ad6f9d0d0f8d6fbdb13388e3dbb21da
Amber Farrington <amberf0712@outlook.com>
2016-11-05 19:05:38 -0700

    Update question-tile.js

--- app/components/question-tile.js
+++ app/components/question-tile.js
@@ -1,9 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  favoriteQuestions: Ember.inject.service(),
   actions: {
     favoriteQuestionHandler(question) {
-    this.sendAction('favoriteQuestionHandler', question);
+     this.get('favoriteQuestions').add(question);
   }
   },
 
